2ceafb3cb34c04d05be5455088d65f70df85e962
Enny1991 <enea.ceolini@gmail.com>
2013-10-07 23:57:05 +0200

    aggiunto campo

--- BlueBattleship/gen/com/et/bluebattleship/R.java
+++ BlueBattleship/gen/com/et/bluebattleship/R.java
@@ -35,16 +35,19 @@ public final class R {
         public static final int textView1=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int text=0x7f030001;
+        public static final int activity_campo=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int text=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int campo=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_campo=0x7f050003;
     }
     public static final class style {
         /** 
